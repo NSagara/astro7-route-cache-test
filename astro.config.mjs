681eb4cd843ba4bc5ad724fbc62fbb7beffd1392
NagahideSagara <nagahide_sagara@yahoo.co.jp>
2026-07-07 09:34:29 +0900

    Initial Astro 7 route caching test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
+import { cacheVercel } from '@astrojs/vercel/cache';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel(),
+  cache: {
+    provider: cacheVercel()
+  }
+});
