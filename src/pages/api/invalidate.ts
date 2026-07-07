22037001636cc5333f92adc4d9a4dd4eab2f5664
NagahideSagara <nagahide_sagara@yahoo.co.jp>
2026-07-07 09:59:24 +0900

    find: invalidate API作成

---
+++ src/pages/api/invalidate.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const POST: APIRoute = async ({ cache }) => {
+  if (!cache.enabled) {
+    return new Response('Cache is not enabled', {
+      status: 500,
+    });
+  }
+
+  await cache.invalidate({
+    tags: ['test'],
+  });
+
+  return new Response('Cache invalidated');
+};
